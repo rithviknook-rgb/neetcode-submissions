5c5fc2bd6c9cea36a9caed1a7584b13f34b2ef98
Rithvik Devarasetty <rithviknook@gmail.com>
2026-07-10 09:30:21 +0530

    Improve handling of duplicates in three-integer sum
    Refactor logic to handle duplicate values when finding three integers that sum to zero.

--- Data Structures & Algorithms/three-integer-sum/submission-2.cpp
+++ Data Structures & Algorithms/three-integer-sum/submission-2.cpp
@@ -21,9 +21,13 @@ private:
 
             if(sum<0){
                 left++;
+                /*while (left < right && nums[left] == nums[left - 1])
+                left++;*/
             }
             else if(sum>0){
                 right--;
+                /*while (left < right && nums[right] == nums[right + 1])
+                right--;*/
             }
             else{
                 result.push_back({nums[i],nums[left++],nums[right--]});
