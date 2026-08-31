a7e915cd4e668bc0e2931d17e68a244b7bfa9710
Rithvik Devarasetty <rithviknook@gmail.com>
2026-08-31 20:24:02 +0530

    Add: adapter - submission-0

---
+++ Design Patterns/adapter/submission-0.cpp
@@ -0,0 +1,47 @@
+class Square {
+    double sideLength;
+
+public:
+    Square() : sideLength(0) {}
+
+    Square(double sideLength) : sideLength(sideLength) {}
+
+    virtual double getSideLength() {
+        return sideLength;
+    }
+};
+
+class SquareHole {
+    double sideLength;
+
+public:
+    SquareHole(double sideLength) : sideLength(sideLength) {}
+
+    bool canFit(Square& square) {
+        return sideLength >= square.getSideLength();
+    }
+};
+
+class Circle {
+    double radius;
+
+public:
+    Circle(double radius) : radius(radius) {}
+
+    double getRadius() {
+        return radius;
+    }
+};
+
+class CircleToSquareAdapter : public Square {
+private:
+    Circle& circle;
+public:
+    CircleToSquareAdapter(Circle& circle) : circle(circle){
+      // Write your code here
+    }
+
+    double getSideLength() override {
+      return 2 * circle.getRadius();
+    }
+};
